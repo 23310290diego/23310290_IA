ff3b335e36b5d9932c638f5e0bc67f14dbfc8384
23310290diego <a23310290@ceti.mx>
2026-03-03 23:59:14 -0600

    Create 3_ Caracteres de escape y nueva línea en Python.py

---
+++ 3_ Caracteres de escape y nueva línea en Python.py
@@ -0,0 +1,10 @@
+# Imprime el texto "La Witsi Witsi Araña"
+# \n significa salto de línea (nueva línea)
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+
+# print() vacío deja una línea en blanco
+print()
+
+# Imprime la segunda parte de la canción
+# Nuevamente usamos \n para dividir el texto en dos líneas
+print("Vino la lluvia\ny se la llevó.")
