1506d2554b65bac3f14f5d9dd87f19c1bf7d05a4
23310290diego <a23310290@ceti.mx>
2026-03-04 20:22:29 -0600

    Create 7_LAB literales de python.py

---
+++ 7_LAB literales de python.py
@@ -0,0 +1,8 @@
+
+#2.2.6
+# # Imprime el texto usando:
+# \"  → para mostrar comillas dentro de la cadena
+# \n  → para hacer saltos de línea
+# Todo se hace en una sola llamada a print()
+
+print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")
