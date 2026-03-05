21056b5e0ab6347e4d71b667bc485e152312d71c
23310290diego <a23310290@ceti.mx>
2026-03-04 19:24:35 -0600

    enteros, octales y hexadecimales

---
+++ 5_enteros octales y hexadecimales.py
@@ -0,0 +1,24 @@
+#2.2.2
+# Enteros básicos (sin parte decimal)
+print(10)        # entero positivo
+print(-25)       # entero negativo
+print(+100)      # el signo + es opcional
+
+# Uso de guion bajo para mejorar lectura (Python 3.6+)
+print(11_111_111)  # Python ignora los "_"
+
+# Número negativo con guion bajo
+print(-11_111_111)
+
+#octales
+# 0o Número octal (base 8)
+# Empieza con 0o y solo permite dígitos del 0 al 7
+# 0o123 significa:
+# 1×8² + 2×8¹ + 3×8⁰ = 83
+print(0o123)     # imprime 83 en decimal
+
+# Hexadecimal (base 16)
+# Empieza con 0x y los dígitos van de 0–9 y A–F
+# 0x123 significa:
+# 1×16² + 2×16¹ + 3×16⁰ = 291
+print(0x123)  # imprime 291
