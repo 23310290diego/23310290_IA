078fd5b9bb6309e2d8b41d63b7f635ac93775832
23310290diego <a23310290@ceti.mx>
2026-03-04 20:43:59 -0600

    Create 8_python como calculadora.py

---
+++ 8_python como calculadora.py
@@ -0,0 +1,20 @@
+# Suma (+)
+print(5 + 3)   # 8
+
+# Resta (-)
+print(10 - 4)  # 6
+
+# Multiplicación (*)
+print(6 * 2)   # 12
+
+# División normal (/)
+print(7 / 2)   # 3.5 → siempre devuelve float
+
+# División entera (//)
+print(7 // 2)  # 3 → solo la parte entera
+
+# Módulo (%) → residuo de la división
+print(7 % 2)   # 1
+
+# Exponente (**)
+print(2 ** 3)  # 8 → 2 elevado a la 3
