f3e27eec2dc2cbdedb87b9fc3e8f07c23ddc0b16
23310290diego <a23310290@ceti.mx>
2026-03-06 15:25:12 -0600

    Update C1_10_comentarios.py

--- C1_10_comentarios.py
+++ C1_10_comentarios.py
@@ -1,4 +1,17 @@
-a = 6
-b = 3
-a /= 2 * b
-print(a)
+# Este programa calcula los segundos en cierto número de horas
+# Este programa fue escrito hace dos días
+
+a = 2  # número de horas
+
+seconds = 3600  # número de segundos que tiene una hora
+
+print("Horas:", a)  # imprime el número de horas almacenado en la variable a
+
+# Esta línea multiplicaría las horas por los segundos que tiene una hora
+# para obtener el total de segundos
+# print("Segundos en Horas:", a * seconds)
+
+# Aquí también se debería imprimir un mensaje de despedida,
+# pero el programador no tuvo tiempo de escribirlo
+
+# Este es el fin del programa que calcula el número de segundos en 2 horas
