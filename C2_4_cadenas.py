3da2bf76b29debd7b72ca6b848849b23091fb9ae
23310290diego <a23310290@ceti.mx>
2026-03-15 19:56:08 -0600

    Update C2_4_cadenas.py

--- C2_4_cadenas.py
+++ C2_4_cadenas.py
@@ -65,3 +65,49 @@ for ix in range(len(the_string)):
     print(the_string[ix], end=' ')
 
 print() # Solo para dar un salto de línea al final
+
+print("\n")
+print("Rebanadas")
+
+alpha = "abdefg"
+
+# [inicio : fin] -> El "fin" no se incluye
+print(alpha[1:3])    # Salida: bd (del índice 1 al 2)
+
+# [inicio : ] -> Desde el inicio hasta el final
+print(alpha[3:])     # Salida: efg (del índice 3 hasta acabar)
+
+# [ : fin] -> Desde el principio hasta antes del fin
+print(alpha[:3])     # Salida: abd (del inicio al índice 2)
+
+# Con números negativos (contando desde atrás)
+print(alpha[3:-2])   # Salida: e (del índice 3 hasta antes de los últimos 2)
+print(alpha[-3:4])   # Salida: e (del tercero de atrás para adelante hasta el índice 4)
+
+# [inicio : fin : paso]
+print(alpha[::2])    # Salida: adf (salta de 2 en 2: a, d, f)
+print(alpha[1::2])   # Salida: beg (empieza en 1 y salta de 2 en 2: b, e, g)
+
+print("\n")
+print("Operadores in y not in")
+print("Operadores in")
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# El operador 'in' busca si un texto está dentro de otro
+print("f" in alphabet)      # True (la f sí está)
+print("F" in alphabet)      # False (Python distingue entre mayúsculas y minúsculas)
+print("1" in alphabet)      # False (el 1 no es parte del abecedario)
+print("ghi" in alphabet)    # True (puedes buscar pedazos completos de texto)
+print("Xyz" in alphabet)    # False (la X mayúscula hace que no coincida)
+
+print("Operador not in")
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# 'not in' devuelve True si el texto NO se encuentra adentro
+print("f" not in alphabet)      # False (porque la 'f' SÍ está)
+print("F" not in alphabet)      # True (porque la 'F' mayúscula NO está)
+print("1" not in alphabet)      # True (porque el '1' NO está)
+print("ghi" not in alphabet)    # False (porque 'ghi' SÍ está)
+print("Xyz" not in alphabet)    # True (porque 'Xyz' con mayúscula NO está)
