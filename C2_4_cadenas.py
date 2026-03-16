0f5ff164e8b063d5fd98b044d7f13480cf67eca0
23310290diego <a23310290@ceti.mx>
2026-03-15 19:50:33 -0600

    Update C2_4_cadenas.py

--- C2_4_cadenas.py
+++ C2_4_cadenas.py
@@ -39,3 +39,29 @@ print(str2 + str1)  # Salida: ba
 # Replicación (Multiplicar cadenas): Repite el texto varias veces
 print(5 * 'a')      # Salida: aaaaa
 print('b' * 4)      # Salida: bbbb
+
+print("\n")
+print("Funcion ord y chr")
+
+# ord() hace el camino de IDA: de letra a número
+char_1 = 'a'
+char_2 = ' '  # Un espacio también es un carácter
+print(ord(char_1)) # Salida: 97
+print(ord(char_2)) # Salida: 32
+
+# chr() hace el camino de VUELTA: de número a letra
+print(chr(97))     # Salida: a
+print(chr(945))    # Salida: α
+
+print("\n")
+print("Cadenas como secuencias")
+
+the_string = 'silly walks'
+
+# Usamos len() para saber cuántas letras hay y range() para generar los índices
+for ix in range(len(the_string)):
+    # Imprimimos la letra que está en la posición 'ix'
+    # end=' ' hace que las letras salgan seguidas en lugar de una por línea
+    print(the_string[ix], end=' ')
+
+print() # Solo para dar un salto de línea al final
