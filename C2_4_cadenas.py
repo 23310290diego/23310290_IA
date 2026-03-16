0a455cd86e88b2f0ec28a88356ded044e56d4176
23310290diego <a23310290@ceti.mx>
2026-03-15 19:44:30 -0600

    Create C2_4_cadenas.py

---
+++ C2_4_cadenas.py
@@ -0,0 +1,16 @@
+# Ejemplo 1: ¿Cuántas letras hay?
+word = 'by'
+print(len(word))  # Salida: 2 (la 'b' y la 'y')
+
+
+# Ejemplo 2: Cadena vacía
+empty = ''
+print(len(empty)) # Salida: 0 (no hay nada adentro)
+
+
+# Ejemplo 3:
+# La \ no cuenta porque es solo un aviso para Python.
+# \' significa una comilla
+i_am = 'I\'m'
+print(len(i_am))  # Salida: 3 (la 'I', la comilla y la 'm')
+
